ee026da39db71180f70b715212fd3dc9a6f9b042
Toni Barth <software@satoprogs.de>
2018-12-17 14:11:15 +0100

    began implementing genetic algorithms

---
+++ graph-coloring/graph-coloring/algorithms/GeneticAlgorithm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using graph_coloring;
+using graph_coloring.solutions;
+
+namespace graph_coloring.algorithms
+{
+  public class GeneticAlgorithm : Algorithm
+  {
+
+    private const int Amount_Start_Solutions = 100;
+
+    public GeneticAlgorithm(Graph g) : base(g, "genetic")
+    {
+    }
+
+    public override Solution Run()
+    {
+      List<GeneticSolution> start_solutions = new List<GeneticSolution>(Amount_Start_Solutions);
+      int i,j,r;
+      int[] c;
+      double limit_w = 2.0*this.graph.EdgeCount*this.graph.NodeCount;
+      List<GeneticSolution> neighbors = new List<GeneticSolution>(Amount_Start_Solutions);
+
+      for(i=0; i < Amount_Start_Solutions; i++)
+      {
+        c = new int[this.graph.NodeCount];
+        for(j=0; j < this.graph.NodeCount; j++)
+        {
+          r = Randomizer.Next(this.graph.NodeCount) + 1;
+          c[j] = r;
+        }
+        start_solutions.Add(new GeneticSolution(this.graph, c));
+      }
+
+      while(true)
+      {
+        neighbors.Clear();
+        for(i=0; i < Amount_Start_Solutions; i++)
+        {
+          var enumerator = start_solutions[i].GetNextNeighbor().GetEnumerator();
+          enumerator.MoveNext();
+          neighbors.Add((GeneticSolution)enumerator.Current);
+        }
+      }
+
+      return null;
+    }
+  }
+}
---
+++ graph-coloring/graph-coloring/solutions/GeneticSolution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using graph_coloring;
+
+namespace graph_coloring.solutions
+{
+  public class GeneticSolution : LocalSearchSolution
+  {
+
+    public GeneticSolution(Graph g, int[] c) : base(g, c)
+    {
+    }
+
+    public override IEnumerable<object> GetNextNeighbor()
+    {
+      yield return null;
+    }
+  }
+}
