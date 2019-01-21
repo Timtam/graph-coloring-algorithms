40991affcb165e552d28152092a46c1199ca91d8
Toni Barth <software@satoprogs.de>
2019-01-21 22:46:38 +0100

    added stub files for branch bound algorithm

---
+++ graph-coloring/graph-coloring/algorithms/BranchBoundAlgorithm.cs
@@ -0,0 +1,26 @@
+﻿using graph_coloring;
+using graph_coloring.solutions;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace graph_coloring.algorithms
+{
+  public class BranchBoundAlgorithm : Algorithm
+  {
+    public BranchBoundAlgorithm(Graph g) : base(g, "branch-bound")
+    {
+    }
+
+    public override Solution Run()
+    {
+      return null;
+    }
+
+    public override void SetTimeout(int t)
+    {
+      return;
+    }
+  }
+}
---
+++ graph-coloring/graph-coloring/solutions/BranchBoundSolution.cs
@@ -0,0 +1,22 @@
+﻿using graph_coloring;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace graph_coloring.solutions
+{
+  public class BranchBoundSolution : Solution
+  {
+    public BranchBoundSolution(Graph g, int[] colors) : base(g, colors)
+    {
+    }
+
+    public override IEnumerable<object> GetNextNeighbor()
+    {
+      yield return null;
+    }
+  }
+}
